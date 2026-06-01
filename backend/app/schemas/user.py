1bc67bd7ee2b71ec56ebec1d575469463f185539
unknown <>
2026-06-01 20:01:21 -0300

    Começa a fazer o 'seed.py' para testar dados no BD. Faz a função de criar usuário em repository_user.py

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -2,12 +2,17 @@ from datetime import datetime
 from pydantic import BaseModel, EmailStr
 from app.models.user import UserRole
 
-class UserResponse(BaseModel):
-    matricula: str
+class UserBase(BaseModel):
     nome: str
     email: EmailStr
     role: UserRole
     ativo: bool
+
+class TicketCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    matricula: str
     created_at: datetime
 
     model_config = {"from_attributes": True}
